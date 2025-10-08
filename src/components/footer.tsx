ccc8c85e3f2845581af1d6cf64afd72141aeb7cb
Dan Nguyen <Dan_Nguyen1@student.uml.edu>
2025-10-08 18:41:40 -0400

    add clsx, move footer and navbar to components, add routes.ts

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+
+export default function Footer() {
+    return (
+      <>
+        {/*Footer for social media links eventually */}
+        <footer className="w-full bg-gray-100 text-center py-6 text-sm text-gray-500">
+          Put social media stuff down here *placeholder for now*
+        </footer>
+      </>
+    );
+ }
---
+++ src/components/routes.ts
@@ -0,0 +1,22 @@
+export const routes = [
+    {
+        key: 'home',
+        path: '/home',
+        name: 'Home',
+    },
+    {
+        key: 'news',
+        path: '/news',
+        name: 'News',
+    },
+    {
+        key: 'people',
+        path: '/people',
+        name: 'People',
+    },
+    {
+        key: 'contact',
+        path: '/contact',
+        name: 'Contact',
+    },
+];
