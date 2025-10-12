4067072a44fa8c6fcc212729f7e0753d9831a664
EthanPKittell <ekittell20@gmail.com>
2025-10-11 21:56:29 -0400

    Added footer link to socials
    I added icons that will act as links to ARA lab socials in the future. Trying to keep this website looking as similar to the wireframe as possible and thought this would look nice.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,55 @@
 export default function Footer() {
     return (
       <>
-        {/*Footer for social media links eventually */}
-        <footer className="w-full bg-gray-100 text-center py-6 text-sm text-gray-500">
-          Put social media stuff down here *placeholder for now*
-        </footer>
+       <footer className="w-full bg-gray-100 text-center py-6">
+        <div className="flex justify-center space-x-6">
+          {/*link to X social media*/}
+          <a
+            href="https://x.com"/*placeholder link for now*/
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="footer-icon"
+          >
+            <img src="/icons/XIcon.png" alt="X" className="icon" />
+          </a>
+
+          {/*link to instagram for lab*/}
+          <a
+            href="https://www.instagram.com" /*placeholder link for now*/
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer-icon" 
+          >
+            <img src="/icons/instaIcon.png" alt="Instagram Icon" className="icon" />
+          </a>
+          
+          {/*link to youtube for lab*/}
+          <a
+            href="https://www.youtube.com"/*placeholder link for now*/
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer-icon"
+          >
+            <img src="/icons/youtubeIcon.png" alt="YouTube" className="icon" />
+          </a>
+
+          {/*link to the ARA lab linkedin*/}
+          <a
+            href="https://www.linkedin.com"/*placeholder link for now*/
+            target="_blank"
+            rel="noopener noreferrer" 
+            className="footer-icon"
+          >
+            <img src="/icons/linkedinIcon.png" alt="LinkedIn" className="icon" />
+          </a> 
+
+          
+        </div>
+
+        <p className="text-sm text-gray-500 mt-4">
+          © {new Date().getFullYear()} ARA Lab. 
+        </p>
+    </footer>
       </>
     );
  }
