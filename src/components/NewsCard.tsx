07c702a8cb1bfcaf20fead7e9b15a4afce0af20f
varadn <varadn@users.noreply.github.com>
2025-11-09 18:41:17 -0500

    Revert "adding delete"
    This reverts commit 4a3273809ef15ec3c4923967fc33c3c2b7aee11f.

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import { convertGoogleDriveUrl } from '@/utils/helpers';
-import { useAuth } from '@/contexts/AuthContext';
 
 interface NewsCardProps {
-  id: number;
   title: string; 
   location: string;
   date: string;
@@ -11,7 +9,6 @@ interface NewsCardProps {
   imageSrc: string;
   imageAlt?: string;
   writtenBy: string;
-  onDelete?: () => void;
 }
 
 export default function NewsCard({
@@ -21,10 +18,8 @@ export default function NewsCard({
   description, 
   imageSrc,
   imageAlt = "News image",
-  writtenBy,
-  onDelete
+  writtenBy
 }: NewsCardProps) {
-  const { user } = useAuth();
 
     let dateLocation: string = date;
 
@@ -44,19 +39,11 @@ export default function NewsCard({
             />
         </div> 
 
-      <div className="text-left relative w-full">
+      <div className="text-left">
         <h2 className="text-xl font-semibold text-gray-800 mb-1">{title}</h2>
         <p className="text-md text-gray-600 mb-2 font-semibold">{dateLocation}</p>
         <p className="text-sm text-gray-600 mb-2 font-semibold">Author: {writtenBy}</p>
         <p className="text-gray-700">{description}</p>
-        {user && onDelete && (
-          <button
-            onClick={onDelete}
-            className="absolute top-0 right-0 bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition-colors"
-          >
-            Delete
-          </button>
-        )}
       </div>
     </div>
   );
