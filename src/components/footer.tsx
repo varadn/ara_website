96541d21e90e19ef458bc08a010a69299e4260d4
EthanPKittell <ekittell20@gmail.com>
2025-12-04 23:10:19 -0500

    Links at footer now do something
    Prof Cabrera wanted these social media buttons to link to these accounts (confirmed on slack)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,9 @@ export default function Footer() {
        <footer className="w-full bg-gray-100 text-center py-6" role="contentinfo" aria-label="Footer">
         <div className="flex justify-center space-x-6" role="list" aria-label="Social media links">
           {/*link to X social media*/}
+          {/*We dont need this 
           <a
-            href="https://x.com"/*placeholder link for now*/
+            href="https://x.com"
             target="_blank" 
             rel="noopener noreferrer"
             className="footer-icon"
@@ -14,11 +15,11 @@ export default function Footer() {
             role="listitem"
           >
             <img src="/icons/XIcon.png" alt="" className="icon" aria-hidden="true" />
-          </a>
+          </a>*/}
 
           {/*link to instagram for lab*/}
           <a
-            href="https://www.instagram.com" /*placeholder link for now*/
+            href="https://www.instagram.com/minerschool_cs_uml/?hl=en" /*placeholder link for now*/
             target="_blank"
             rel="noopener noreferrer"
             className="footer-icon"
@@ -29,8 +30,9 @@ export default function Footer() {
           </a>
           
           {/*link to youtube for lab*/}
+          {/*Dont need this for now
           <a
-            href="https://www.youtube.com"/*placeholder link for now*/
+            href="https://www.youtube.com"
             target="_blank"
             rel="noopener noreferrer"
             className="footer-icon"
@@ -39,10 +41,11 @@ export default function Footer() {
           >
             <img src="/icons/youtubeIcon.png" alt="" className="icon" aria-hidden="true" />
           </a>
+          */}
 
           {/*link to the ARA lab linkedin*/}
           <a
-            href="https://www.linkedin.com"/*placeholder link for now*/
+            href="https://www.linkedin.com/company/kennedy-college-of-sciences-at-umass-lowell/"/*placeholder link for now*/
             target="_blank"
             rel="noopener noreferrer"
             className="footer-icon"
